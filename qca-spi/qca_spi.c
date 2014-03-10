778e16bce81e2d5e6647670715fae319ed54bb9a
Stefan Wahren <stefan.wahren@i2se.com>
2014-03-10 22:53:20 +0100

    Remove unnecessary include
    Signed-off-by: Stefan Wahren <stefan.wahren@i2se.com>

--- qca-spi/qca_spi.c
+++ qca-spi/qca_spi.c
@@ -38,7 +38,6 @@
 #include <linux/of.h>
 #include <linux/of_device.h>
 #include <linux/of_gpio.h>
-#include <linux/platform_device.h>
 #include <linux/sched.h>
 #include <linux/skbuff.h>
 #include <linux/spi/spi.h>
