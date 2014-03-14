8daaac1d32a73d2ed58b5ffa43a18045947a6765
Stefan Wahren <stefan.wahren@i2se.com>
2014-03-14 09:46:43 +0100

    Add driver stats via debugfs
    Signed-off-by: Stefan Wahren <stefan.wahren@i2se.com>

--- qca-spi/qca_debug.c
+++ qca-spi/qca_debug.c
@@ -66,13 +66,41 @@ qcaspi_regs_dump(struct seq_file *s, void *what)
 }
 
 static int
+qcaspi_stats_show(struct seq_file *s, void *what)
+{
+	struct qcaspi *qca = s->private;
+
+	seq_printf(s, "Triggered resets    : %lu\n", qca->stats.trig_reset);
+	seq_printf(s, "Device resets       : %lu\n", qca->stats.device_reset);
+	seq_printf(s, "Reset timeouts      : %lu\n", qca->stats.reset_timeout);
+	seq_printf(s, "Read buffer errors  : %lu\n", qca->stats.read_buf_err);
+	seq_printf(s, "Write buffer errors : %lu\n", qca->stats.write_buf_err);
+	seq_printf(s, "Out of memory       : %lu\n", qca->stats.out_of_mem);
+
+	return 0;
+}
+
+static int
 qcaspi_regs_open(struct inode *inode, struct file *file)
 {
 	return single_open(file, qcaspi_regs_dump, inode->i_private);
 }
 
+static int
+qcaspi_stats_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, qcaspi_stats_show, inode->i_private);
+}
+
 static const struct file_operations qcaspi_regs_ops = {
 	.open = qcaspi_regs_open,
+	.read = seq_read,
+	.llseek = seq_lseek,
+	.release = single_release,
+};
+
+static const struct file_operations qcaspi_stats_ops = {
+	.open = qcaspi_stats_open,
 	.read = seq_read,
 	.llseek = seq_lseek,
 	.release = single_release,
@@ -93,6 +121,9 @@ qcaspi_init_device_debugfs(struct qcaspi *qca)
 	}
 	debugfs_create_file("regs", S_IFREG | S_IRUGO, device_root, qca,
 			&qcaspi_regs_ops);
+
+	debugfs_create_file("stats", S_IFREG | S_IRUGO, device_root, qca,
+			&qcaspi_stats_ops);
 }
 
 void
